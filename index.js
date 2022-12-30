83bc9bdc3615a93e5126f781c41ef7ba8e40ecd5
brunash <bobazso@yahoo.com>
2022-12-30 13:39:59 -0500

    setting up back end

---
+++ index.js
@@ -0,0 +1,10 @@
+import Express from "express";
+import userRoutes from "./routes/users.js"
+
+const app = Express();
+
+app.use("/api/users", userRoutes)
+
+app.listen(8800, () => {
+    console.log("API working!")
+}) 
