03bddebee1b572528a0658f3cda0a707a9dfafaa
brunash <bobazso@yahoo.com>
2023-01-08 09:25:41 -0500

    working on functionality

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,11 +1,11 @@
-import {db} from "../connect.js";
+import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
 import moment from "moment";
 
-
 export const getComments = (req, res) => {
   const q = `SELECT c.*, u.id AS userId, name, profilePic FROM comments AS c JOIN users AS u ON (u.id = c.userId)
-  WHERE c.postId =  ?  ORDER BY c.createdAt DESC`;
+    WHERE c.postId = ? ORDER BY c.createdAt DESC
+    `;
 
   db.query(q, [req.query.postId], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -21,8 +21,7 @@ export const addComment = (req, res) => {
     if (err) return res.status(403).json("Token is not valid!");
 
     const q =
-      "INSERT INTO comments (`desc`,`createdAt`,`userId`, `postId`) VALUES (?)";
-
+      "INSERT INTO comments(`desc`, `createdAt`, `userId`, `postId`) VALUES (?)";
     const values = [
       req.body.desc,
       moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
@@ -32,7 +31,7 @@ export const addComment = (req, res) => {
 
     db.query(q, [values], (err, data) => {
       if (err) return res.status(500).json(err);
-      return res.status(200).json("Comment has been added.");
+      return res.status(200).json("Comment has been created.");
     });
   });
 };
