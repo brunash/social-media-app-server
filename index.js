044466962a5cd6e7de9e33c1e52db6e567134183
brunash <bobazso@yahoo.com>
2023-02-01 21:07:54 -0500

    working on functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
+import relationshipRoutes from "./routes/relationships.js"
 import likeRoutes from "./routes/likes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -46,6 +47,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
+app.use("/api/relationships", relationshipRoutes);
 
 app.listen(8800, () => {
     console.log("API working!")
